9c2af5c141b9590a090eefac2012e47bafa791cf
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2017-11-10 23:56:15 +0100

    Added test code

--- arduino venster.py
+++ arduino venster.py
@@ -1,3 +1,5 @@
+import serial
+import time
 """
 
 klasse arduino vanuit  oogpunt van layout
@@ -30,6 +32,54 @@ class Arduino:
         self.name = name
         self.port = port
         self.listLight = listLight
+        self.ser = serial.Serial(port=port, baudrate=19200, timeout=1)
+
+    # Read output from Arduino
+    def readArduino(self):
+        # Close all connections
+        self.ser.close()
+        # Declare stop sign
+        stop = (b'\n').decode('ascii')
+        # Try to open a connection with ser
+        try:
+            self.ser.open()
+            open = True
+        except:
+            open = False
+        # Declare string for return statement
+        word = ''
+        # Read output if connection is opened
+        if (open):
+            # Read from ser
+            while (open):
+                # Try to read from ser
+                try:
+                    read = self.ser.read().decode('ascii')
+                    # Return word if stop sign was given
+                    if (read == stop):
+                        return word
+                        exit()
+                    # Update word with next character
+                    else:
+                        word += read
+                except Exception:
+                    # Give error if connection failed
+                    error = 'Cannot open serial port'
+                    return error
+
+    # Write to Arduino
+    def writeArduino(self, command):
+        # Read output if connection is opened
+        if (open):
+            self.ser.write((command + "\n").encode('ascii'))
+            extra_info = None
+            l = self.ser.readline().decode('ascii').strip()
+            if l not in ["OK", "ERR"]:
+                extra_info = l
+                l = self.ser.readline().decode('ascii').strip()
+                if l not in ["OK", "ERR"]:
+                    l = None
+            return (l, extra_info)
 
     def returnName(self):
         return self.name
@@ -53,6 +103,10 @@ class Arduino:
         return self.status
 
 
-test = Arduino('test', '2', listLight[1])
+test = Arduino('test', 'COM4', listLight[1])
 
-print(test.returnStatus())
+test.writeArduino('Status')
+test.writeArduino('Status')
+test.writeArduino('Rol down')
+time.sleep(2)
+test.writeArduino('Rol up')
