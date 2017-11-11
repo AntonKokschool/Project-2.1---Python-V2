c101b005108a1039a9b213bb33cb33f214c0a31c
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2017-11-11 12:55:17 +0100

    Added write and open port functions. Renamed file.

--- arduinoClass.py
+++ arduinoClass.py
@@ -1,51 +1,58 @@
 import serial
-port = 'COM4'
+import time
+"""
 
-class Arduino():
+klasse arduino vanuit  oogpunt van layout
 
-    def __init__(self, port):
+    mockup voorbeeld                                     wat moet het doen      done/moeilijkheden
+    poort weergeven                                      return                 gedaan
+    naam weergeven                                       return                 gedaan
+    oprol                                                static variable        weet niet hoe static werkt
+    uitrol de maximale uitrol afstand                    aanpasbare variable    gedaan
+    status waar is de arduino mee bezig                  return                 gedaan
+    licht kan 3 waarden hebben "bright, light or dim"    return                 gedaan
+    temperatuur weergaven                                return                 gedaan
+    automatisch                                          ????                   ik weet niet hoe dit eruit moet zien
+    handmatig                                            ????                   ik weet niet hoe dit eruit moet zien
+    vergrendelen                                         ????                   ik weet niet hoe dit eruit moet zien
+
+"""
+
+listLight = ["bright", "light", "dim"]
+
+
+class Arduino:
+
+    rollUp = 0.05
+    rollDown = 1
+    tempeture = 20
+    status = ""
+
+    def __init__(self, name, port, listLight):
+        self.name = name
         self.port = port
+        self.listLight = listLight
         self.ser = serial.Serial(port=port, baudrate=19200, timeout=1)
 
-    # Read output from Arduino
-    def readArduino(self):
-        # Close all connections
+    #
+    def openConnection(self):
+        # Close connection
         self.ser.close()
-        # Declare stop sign
-        stop = (b'\n').decode('ascii')
         # Try to open a connection with ser
         try:
             self.ser.open()
             open = True
         except:
             open = False
-        # Declare string for return statement
-        word = ''
-        # Read output if connection is opened
-        if (open):
-            # Read from ser
-            while (open):
-                # Try to read from ser
-                try:
-                    read = self.ser.read().decode('ascii')
-                    # Return word if stop sign was given
-                    if (read == stop):
-                        return word
-                        exit()
-                    # Update word with next character
-                    else:
-                        word += read
-                except Exception:
-                    # Give error if connection failed
-                    error = 'Cannot open serial port'
-                    return error
+
+        time.sleep(0.5)
+        return open
+
 
     # Write to Arduino
-    def writeArduino(self, command):
-        # Declare string for return statement
-        word = ''
+    def writeArduino(self, command, connection):
         # Read output if connection is opened
-        if (open):
+        if (connection):
             self.ser.write((command + "\n").encode('ascii'))
             extra_info = None
             l = self.ser.readline().decode('ascii').strip()
@@ -56,14 +63,35 @@ class Arduino():
                     l = None
             return (l, extra_info)
 
-arduino4 = Arduino('COM4')
+    def returnName(self):
+        return self.name
 
-triedLeft = 5
-while (triedLeft > 0):
-    try:
-        r = arduino4.writeArduino('Status')
-        if (r == ('OK','Rolled up')):
-            print(r)
-            triedLeft = 0
-    except:
-     print('Fail')
+    def returnPort(self):
+        return self.port
+
+    def returnRollUp(self):
+        return self.rollUp
+
+    def returnRollDown(self):
+        return self.rollDown
+
+    def returnLight(self):
+        return self.listLight
+
+    def returnTempeture(self):
+        return self.tempeture
+
+    def returnStatus(self):
+        triesLeft = 5
+        while (triesLeft > 0):
+            try:
+                ret = self.writeArduino('Status')
+                if (ret == ('OK', 'Rolled up')):
+                    self.status = 'Rolled up'
+                elif (ret == ('OK', 'Rolled down')):
+                    self.status = 'Rolled down'
+                else:
+                    triesLeft -= 1
+            except:
+                print('Error')
+        return self.status
